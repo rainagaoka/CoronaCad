268435f637fde4ef9ae1fa7125c7a71ff7e0d90f
rainagaoka <59065182+rainagaoka@users.noreply.github.com>
2020-04-07 12:01:53 -0300

    Criada as primeiras linhas.

---
+++ CoronaCad/Mapa.vb
@@ -0,0 +1,14 @@
+﻿Imports Autodesk.AutoCAD.DatabaseServices
+Imports Autodesk.AutoCAD.Runtime
+Imports acaApp = Autodesk.AutoCAD.ApplicationServices.Application
+Public Class Mapa
+    <CommandMethod("CoronaCad")>
+    Public Sub CoronaCad()
+
+        Dim doc = acaApp.DocumentManager.MdiActiveDocument
+
+        Using tr As Transaction = doc.TransactionManager.StartOpenCloseTransaction
+
+        End Using
+    End Sub
+End Class
